406dc679d244887a359504ea36382e25b5e3fca0
Sebastian Contreras <js.contreras@outlook.com>
2024-02-01 16:01:26 -0600

    Sharp testing.

--- app/route-groups/(checkout)/checkout/page.tsx
+++ app/route-groups/(checkout)/checkout/page.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
+function ImageProcessing({ image }: { image: string }) {
+  const processedImage = sharp(image).resize(500, 500).jpeg().toBuffer();
+
+  return <Image src={processedImage} alt="Testing Sharp" />;
+}
+
 export default function Page() {
-  return <h1 className="text-xl font-medium text-gray-400/80">Checkout</h1>;
+  return (
+    <>
+      <ImageProcessing image={'./vercel_logo.jpg'} />
+      <h1 className="text-xl font-medium text-gray-400/80">Checkout</h1>;
+    </>
+  );
 }
