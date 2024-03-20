2c68ad06d05006682f423d971167d0d052df992f
Sebastian Contreras <js.contreras@outlook.com>
2024-03-20 10:10:08 -0500

    Adding toolbar for produciton.

--- ui/vercel-toolbar-ui.tsx
+++ ui/vercel-toolbar-ui.tsx
@@ -1,7 +1,27 @@
 'use client';
 import { VercelToolbar } from '@vercel/toolbar/next';
+import { useEffect, useState } from 'react';
 
 export function VercelToolbarUI() {
-  const isEmployee = true;
-  return isEmployee ? <VercelToolbar /> : null;
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleKeyDown = (event: any) => {
+      // Check if Ctrl key is pressed along with 'K'
+      if (event.ctrlKey && event.key === 'k') {
+        event.preventDefault(); // Prevent the default action of Ctrl+K
+        setIsVisible(!isVisible); // Toggle visibility
+      }
+    };
+
+    // Add event listener
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Remove event listener on cleanup
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isVisible]);
+
+  return isVisible ? <VercelToolbar /> : null;
 }
