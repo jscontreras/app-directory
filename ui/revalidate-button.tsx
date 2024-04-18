f30606a4ba38bde48d9c144be94ed0d75ae64dec
Sebastian Contreras <js.contreras@outlook.com>
2024-04-18 10:10:52 -0500

    Invalidate via tag via path ready.

--- ui/revalidate-button.tsx
+++ ui/revalidate-button.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { revalidatePathCahe } from '#/lib/actions';
+import { useState } from 'react';
 
 export const RevalidateButton = ({
   path,
@@ -8,14 +9,21 @@ export const RevalidateButton = ({
   path: string;
   copy: string;
 }) => {
+  const [active, setActive] = useState(true);
   async function btnClickHandler() {
+    if (!active) {
+      return;
+    }
+    setActive(false);
     await revalidatePathCahe(path);
     alert(`Revalidated (${path})`);
     location.reload();
   }
   return (
     <button
-      className="bg-vercel-blue rounded-lg px-3 py-1 text-sm font-medium text-white"
+      className={`${
+        active ? 'bg-vercel-blue' : 'bg-vercel-violet'
+      } rounded-lg px-3 py-1 text-sm font-medium text-white`}
       onClick={btnClickHandler}
     >
       {copy}
--- ui/revalidate-tag-button.tsx
+++ ui/revalidate-tag-button.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { revalidateTagCahe } from '#/lib/actions';
+import { useState } from 'react';
 
 export const RevalidateTagButton = ({
   tag,
@@ -8,14 +9,23 @@ export const RevalidateTagButton = ({
   tag: string;
   copy: string;
 }) => {
+  const [active, setActive] = useState(true);
+
   async function btnClickHandler() {
+    if (!active) {
+      return;
+    }
+    setActive(false);
     await revalidateTagCahe(tag);
     alert(`Revalidated (${tag})`);
     location.reload();
   }
   return (
     <button
-      className="bg-vercel-blue rounded-lg px-3 py-1 text-sm font-medium text-white"
+      disabled={!active}
+      className={`${
+        active ? 'bg-vercel-blue' : 'bg-vercel-violet'
+      } rounded-lg px-3 py-1 text-sm font-medium text-white`}
       onClick={btnClickHandler}
     >
       {copy}
