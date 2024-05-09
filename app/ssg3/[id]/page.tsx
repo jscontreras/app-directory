9824898ffc0520ccdbf7b026b357d610db33990a
Sebastian Contreras <js.contreras@outlook.com>
2024-05-09 09:53:19 -0500

    Adding print for dynamic params

--- app/ssg3/[id]/page.tsx
+++ app/ssg3/[id]/page.tsx
@@ -22,7 +22,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     <div className="grid grid-cols-6 gap-x-6 gap-y-3">
       <div className="col-span-full space-y-3 lg:col-span-4">
         <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
-          {data.title}
+          {`${data.title} [${params.id}]`}
         </h1>
         <p className="line-clamp-3 font-medium text-gray-500">{data.body}</p>
         {/* client component */}
