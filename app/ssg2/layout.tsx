2e09830112fa45a149d14b36807dc7da40173c9b
Sebastian Contreras <js.contreras@outlook.com>
2024-05-09 09:54:38 -0500

    Preparing demo ssg2 and ssg3

--- app/ssg2/layout.tsx
+++ app/ssg2/layout.tsx
@@ -16,10 +16,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="space-y-9">
       <div className="flex flex-wrap items-center gap-2">
-        <Tab path="/ssg2" item={{ text: 'Home' }} />
-        <Tab path="/ssg2" item={{ text: 'Post 1', slug: '1' }} />
-        <Tab path="/ssg2" item={{ text: 'Post 2', slug: '2' }} />
-        <RandomPostTab path="/ssg2" />
+        <Tab path="/ssg2" item={{ text: 'Home' }} prefetch={false} />
+        <Tab
+          path="/ssg2"
+          item={{ text: 'Post 1', slug: '1' }}
+          prefetch={false}
+        />
+        <Tab
+          path="/ssg2"
+          item={{ text: 'Post 2', slug: '2' }}
+          prefetch={false}
+        />
+        {/* <RandomPostTab path="/ssg2" /> */}
       </div>
 
       <div>{children}</div>
