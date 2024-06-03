4b16a902e828f9f67873e307960375512ca1717d
Sebastian Contreras <js.contreras@outlook.com>
2024-06-03 10:01:22 -0500

    Adding dependencies to revalidate tag example.

--- app/revalidate/page.tsx
+++ app/revalidate/page.tsx
@@ -28,7 +28,8 @@ function delay(ms: number) {
 export default async function Page() {
   await delay(9000);
   const res = await fetch(`https://worldtimeapi.org/api/ip`, {
-    next: { tags: ['test-tag'] },
+    cache: 'force-cache',
+    next: { tags: ['test-tag-date'] },
   });
   const data = (await res.json()) as { datetime: string };
   const dateObj = new Date(data.datetime);
@@ -41,9 +42,13 @@ export default async function Page() {
 
   const postId = dateObj.getSeconds();
   // second request
-  const resLorem = await fetch(`https://jsonplaceholder.typicode.com/posts/3`, {
-    next: { tags: ['test-tag-2'] },
-  });
+  const resLorem = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    {
+      cache: 'force-cache',
+      next: { tags: ['test-tag-post'] },
+    },
+  );
 
   const dataLorem = (await resLorem.json()) as { title: string; body: string };
 
@@ -58,7 +63,10 @@ export default async function Page() {
           Time in New York: <span className="text-amber-400">{date}</span>{' '}
           (obtained via fetch)
         </li>
-        <li>Lorem ipsum post (obtained via fetch)</li>
+        <li>
+          Lorem ipsum post (obtained via fetch using the seconds of Time in New
+          York as postId)
+        </li>
         <li>It contains two fetched requests tagged with unique tags.</li>
         <li>
           When invalidating via tags, only one fetch tag is referenced but
@@ -75,10 +83,10 @@ export default async function Page() {
       <div className="mb-8 grid grid-cols-6 gap-x-6 gap-y-3">
         <div className="col-span-full space-y-3 lg:col-span-4">
           <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
-            {dataLorem.title} [{postId}]
+            [{postId}] {dataLorem.title}
           </h1>
           <h3 className="text-gray-200">
-            Last snapshot:{' '}
+            Last snapshot (Vercel Serverless Fn Time):{' '}
             {new Date().toLocaleString('en-US', {
               timeZone: 'America/New_York',
               hour: 'numeric',
