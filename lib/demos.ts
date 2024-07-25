43091cb88510278040288ce315590899992a0916
Sebastian Contreras <js.contreras@outlook.com>
2024-07-25 15:38:06 -0500

    Testing Deployment Summary.

--- lib/demos.ts
+++ lib/demos.ts
@@ -81,7 +81,7 @@ export const demos: { name: string; items: Item[] }[] = [
         description: 'Testing CDN + Tag Cache (draft)',
       },
       {
-        name: 'Static page with Dynamic component',
+        name: 'Static page with Dynamic Query vars',
         slug: 'suspense',
         description:
           'Loads static page and uses query params to interact dinamically with the content',
