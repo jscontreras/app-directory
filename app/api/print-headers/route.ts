bd77a9631ee840b1ab418ca44f8d9b19196b8784
Sebastian Contreras <js.contreras@outlook.com>
2024-08-01 17:06:02 -0500

    Headers API.

---
+++ app/api/print-headers/route.ts
@@ -0,0 +1,15 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  // Convert headers to an object
+  const headers: any = {};
+  request.headers.forEach((value, key) => {
+    headers[key] = value;
+  });
+  delete headers['x-vercel-protection-bypass'];
+  return NextResponse.json({
+    now: Date.now(),
+    cache: 'no-store',
+    headers: headers,
+  });
+}
