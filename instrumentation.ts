f1c8a0df2787d7e9198a322817d13d119ce127ff
Sebastian Contreras <js.contreras@outlook.com>
2024-08-08 16:47:30 -0500

    Instrumentation span edge updated.

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,6 +1,5 @@
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPHttpJsonTraceExporter, registerOTel } from '@vercel/otel';
-// import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 export async function register() {
   if (process.env.TELEMETRY_CUSTOM_PRODUCER === '1') {
@@ -15,23 +14,19 @@ export async function register() {
           },
         },
         attributes: {
-          'highlight.project_id': 'YOUR_PROJECT_ID',
+          'highlight.project_id': process.env.PROJECT_ID,
           'highlight.source': 'backend',
         },
         traceExporter: new OTLPHttpJsonTraceExporter({
           url: 'https://otel.highlight.io:4318/v1/traces',
         }),
-        // spanProcessors: [
-        //   new SimpleSpanProcessor(
-        //     new OTLPTraceExporter({
-        //       url: 'https://your-otel-endpoint/v1/traces',
-        //       headers: {
-        //         Authorization: `Bearer ${process.env.API_TOKEN}`,
-        //         'X-Axiom-Dataset': process.env.DATASET_NAME,
-        //       },
-        //     })
-        //   ),
-        // ],
+        spanProcessors: [
+          new BatchSpanProcessor(
+            new OTLPHttpJsonTraceExporter({
+              url: 'https://otel.highlight.io:4318/v1/traces',
+            }),
+          ),
+        ],
       });
     }
   } else {
