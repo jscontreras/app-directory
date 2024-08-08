a58541d9403440da45078d5067eb07ab2210dbd0
Sebastian Contreras <js.contreras@outlook.com>
2024-08-08 16:12:55 -0500

    Testing metrics.

--- lib/intrumentation.node.ts
+++ lib/intrumentation.node.ts
@@ -3,12 +3,29 @@ import { registerOTel } from '@vercel/otel';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+
 import {
   CompositePropagator,
   W3CBaggagePropagator,
   W3CTraceContextPropagator,
 } from '@opentelemetry/core';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+
+const METRICS_COLLECTOR_STRING = 'https://otlp.nr-data.net:4318/v1/metrics';
+
+const newRelicMetricsExporter: any = new OTLPMetricExporter({
+  url: METRICS_COLLECTOR_STRING,
+  headers: {
+    'api-key': process.env.NEW_RELIC_LICENSE_KEY,
+  },
+});
+
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: newRelicMetricsExporter,
+  exportIntervalMillis: 10000,
+});
 
 // Set up diagnostic logging (optional)
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
@@ -28,6 +45,7 @@ const newRelicExporter = new OTLPTraceExporter({
 const sdk = new NodeSDK({
   serviceName: process.env.NEW_RELIC_APP_NAME,
   traceExporter: newRelicExporter,
+  metricReader: metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
   textMapPropagator: new CompositePropagator({
     propagators: [new W3CTraceContextPropagator(), new W3CBaggagePropagator()],
