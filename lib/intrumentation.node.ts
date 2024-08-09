d4a172d4b0808202c96cb608ee55de06ba6e8f2a
Sebastian Contreras <js.contreras@outlook.com>
2024-08-09 10:40:11 -0500

    Instrumentation.

--- lib/intrumentation.node.ts
+++ lib/intrumentation.node.ts
@@ -1,15 +1,9 @@
 // instrumentation.ts
 import { registerOTel } from '@vercel/otel';
-import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
-import {
-  CompositePropagator,
-  W3CBaggagePropagator,
-  W3CTraceContextPropagator,
-} from '@opentelemetry/core';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 
@@ -41,21 +35,10 @@ const newRelicExporter = new OTLPTraceExporter({
   },
 });
 
-// Initialize the OpenTelemetry SDK
-const sdk = new NodeSDK({
+// Register the OpenTelemetry SDK
+registerOTel({
   serviceName: process.env.NEW_RELIC_APP_NAME,
   traceExporter: newRelicExporter,
   metricReader: metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
-  textMapPropagator: new CompositePropagator({
-    propagators: [new W3CTraceContextPropagator(), new W3CBaggagePropagator()],
-  }),
 });
-
-// Register the OpenTelemetry SDK
-registerOTel({
-  ...sdk,
-  serviceName: process.env.NEW_RELIC_APP_NAME,
-});
-
-sdk.start();
