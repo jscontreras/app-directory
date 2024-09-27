d7fd2dd8b046068527218238b26314c875d2344b
Sebastian Contreras <js.contreras@outlook.com>
2024-09-27 14:29:00 -0500

    Cleaning console logs.

--- app/ssg/[id]/page.tsx
+++ app/ssg/[id]/page.tsx
@@ -1,29 +1,23 @@
-import { printDiskSize } from '#/lib/actions';
 import { notFound } from 'next/navigation';
 
-let memCheck: null | string = null;
-
 export async function generateStaticParams() {
-  printDiskSize();
+  // printDiskSize();
   // Generate two pages at build time and the rest (3-100) on-demand
-  return [{ id: '1' }, { id: '2' }];
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
-  if (memCheck == null) {
-    console.log('### STARING_NEW_LAMBDA ###');
-    memCheck = '### REUSING_LAMBDA###';
-  } else {
-    console.log(memCheck);
-  }
   if (Number(params.id) >= 100) {
     notFound();
   }
+
   const res = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.id}`,
     { next: { revalidate: false } },
   );
+
   const data = (await res.json()) as { title: string; body: string };
+
   console.log(`Rendering from server ssg (compute)[id:${params.id}].`);
   return (
     <div className="grid grid-cols-6 gap-x-6 gap-y-3">
