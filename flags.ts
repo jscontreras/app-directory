56a64d62e53fb4b414f5d85d1551ae3cf0e87272
Sebastian Contreras <js.contreras@outlook.com>
2024-09-27 15:44:29 -0500

    Rebuilding testing ISR.

--- flags.ts
+++ flags.ts
@@ -4,8 +4,8 @@ import { get } from '@vercel/edge-config';
 export const showBottomBar = flag({
   key: 'bottomBar',
   options: [
-    { value: false, label: 'Hide' },
-    { value: true, label: 'Show' } as any,
+    { value: false, label: 'Hide Bar' },
+    { value: true, label: 'Show Bar' } as any,
   ],
   async decide() {
     const { bottomBar } = (await get('flags')) as any; // could use this.key instead
