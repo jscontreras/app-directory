6c4488509448ffe153c51edf3a26e34e7df78217
Sebastian Contreras <js.contreras@outlook.com>
2024-12-02 12:52:18 -0500

    SSG on flags ssg exmple.

--- app/flagged/[code]/layout.tsx
+++ app/flagged/[code]/layout.tsx
@@ -9,7 +9,7 @@ export async function generateStaticParams() {
   return codes.map((code) => ({ code: `${code}` }));
 }
 
-export const revalidate = 60; // Revalidate every 60 seconds
+export const revalidate = false; // Revalidate every 60 seconds
 
 export default async function Layout(props: {
   children: ReactNode;
