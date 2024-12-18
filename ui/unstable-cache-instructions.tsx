e47fffe5a133e58d140442a79f7111c56a827a9c
Sebastian Contreras <js.contreras@outlook.com>
2024-12-18 16:35:15 -0500

    Adding missing space.

--- ui/unstable-cache-instructions.tsx
+++ ui/unstable-cache-instructions.tsx
@@ -22,6 +22,7 @@ export function UnstableCacheInstructions({
           <Highlight colorClass={'text-purple-400'}>revalidate: 20</Highlight>
           {dynamicTag && (
             <>
+              {' '}
               and adding tag{' '}
               <Highlight colorClass={'text-purple-400'}>
                 [{dynamicTag}]
