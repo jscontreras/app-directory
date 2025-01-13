2e331681dd430bd4a4e74bd91947e3efff7a18fd
jscontreras <sebastian.contreras@vercel.com>
2025-01-13 16:00:29 -0500

    Adding id to test page.

--- app/cache-headers/test/page.tsx
+++ app/cache-headers/test/page.tsx
@@ -4,5 +4,5 @@ export default async function Page() {
   // Simulate a 5-second delay
   await new Promise((resolve) => setTimeout(resolve, 5000));
   const timestamp = new Date().toISOString();
-  return <h2>{timestamp}</h2>;
+  return <h2 id="timestamp">{timestamp}</h2>;
 }
