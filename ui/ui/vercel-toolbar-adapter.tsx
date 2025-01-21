f840a5abe8ec766dfe41669b19aded0cb77ce2d5
jscontreras <sebastian.contreras@vercel.com>
2025-01-21 16:15:27 -0500

    Fixing toolbar.

--- ui/ui/vercel-toolbar-adapter.tsx
+++ ui/ui/vercel-toolbar-adapter.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { mountVercelToolbar, unmountVercelToolbar } from '@vercel/toolbar';
 export default function VercelToolbarAdapter() {
-  if (window) {
+  if (typeof window !== 'undefined') {
     (window as any).unmountVercelToolbar = unmountVercelToolbar;
     (window as any).mountVercelToolbar = mountVercelToolbar;
   }
