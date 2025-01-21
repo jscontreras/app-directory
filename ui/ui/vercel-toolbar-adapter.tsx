4243adbb2d439b3871268c1bb935bf8efbb61fba
jscontreras <sebastian.contreras@vercel.com>
2025-01-21 17:51:52 -0500

    Updating Settings to force mount on flags.

--- ui/ui/vercel-toolbar-adapter.tsx
+++ ui/ui/vercel-toolbar-adapter.tsx
@@ -1,9 +1,20 @@
 'use client';
 import { mountVercelToolbar, unmountVercelToolbar } from '@vercel/toolbar';
+
+const settings = {
+  scriptHostName: 'https://vercel.live',
+  projectId: 'prj_EynROlxBukHxBfAbBsef1XEnykEq',
+  branch: 'main',
+  deploymentId: 'dpl_Bxu69LNwdvwvqouB7XHa3h5KKSaE',
+  ownerId: 'team_qt72u6Ug7jZRH1AY3zX9AkUU',
+};
+
 export default function VercelToolbarAdapter() {
   if (typeof window !== 'undefined') {
     (window as any).unmountVercelToolbar = unmountVercelToolbar;
-    (window as any).mountVercelToolbar = mountVercelToolbar;
+    (window as any).mountVercelToolbar = () => {
+      mountVercelToolbar(settings);
+    };
   }
   return null;
 }
