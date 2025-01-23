aedfe10421b1bf62ee368f368556ee1bdf911b72
jscontreras <sebastian.contreras@vercel.com>
2025-01-23 11:46:07 -0500

    Adding <100 condition to ISR demo.

--- app/isr/[id]/page.tsx
+++ app/isr/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getISODateServerAction } from '#/lib/actions';
 import { RenderingInfo } from '#/ui/rendering-info';
+import { notFound } from 'next/navigation';
 
 export const dynamicParams = true;
 
@@ -11,12 +12,18 @@ export async function generateStaticParams() {
   return [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }];
 }
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id}`,
-    { next: { tags: ['collection'] }, cache: 'force-cache' },
-  );
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = Number.parseInt(params.id, 10);
+
+  // Check if id is bigger than 100
+  if (id > 100) {
+    notFound();
+  }
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: { tags: ['collection'] },
+    cache: 'force-cache',
+  });
 
   const currentTime = await getISODateServerAction();
 
@@ -25,9 +32,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   return (
     <div className="grid grid-cols-6 gap-x-6 gap-y-3">
       <div className="col-span-full space-y-3 lg:col-span-4">
-        <h1 className="truncate text-2xl font-medium capitalize text-gray-200">
-          {`[${params.id}] ${data.title}`}
-        </h1>
+        <h1 className="truncate text-2xl font-medium capitalize text-gray-200">{`[${id}] ${data.title}`}</h1>
         <p className="font-medium text-gray-500">{data.body}</p>
         <p className="font-medium text-amber-200">
           Snapshot Date: {currentTime}
