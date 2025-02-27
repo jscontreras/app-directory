fd4ae39443a8dc411eeedc3b2cb06a45ed90be61
jscontreras <sebastian.contreras@vercel.com>
2025-02-27 15:10:49 -0600

    Adding sitemap entry.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,14 +4,22 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   const root = `https://www.tc-vercel.dev/`;
   let priority: number = 1.0;
-  const sample = {
-    url: root,
-    lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: priority,
-  };
+  const samples = [
+    {
+      url: root,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: priority,
+    },
+    {
+      url: `${root}/pocs/images`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: priority,
+    },
+  ];
 
-  const entries: any[] = [sample];
+  const entries: any[] = [...samples];
   demos.forEach((section) => {
     priority = parseFloat((priority - 0.1).toFixed(1));
     section.items.forEach((item) => {
