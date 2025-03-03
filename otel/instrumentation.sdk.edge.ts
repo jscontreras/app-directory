1ae498d69132f0526de6657dc047e62b69207301
jscontreras <sebastian.contreras@vercel.com>
2025-03-03 17:08:34 -0600

    Updating vercel otel exporters.

--- otel/instrumentation.sdk.edge.ts
+++ otel/instrumentation.sdk.edge.ts
@@ -1,6 +1,8 @@
-import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { OTLPHttpJsonTraceExporter, registerOTel } from '@vercel/otel';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
+import { registerOTel, OTLPHttpProtoTraceExporter } from '@vercel/otel';
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+
 // import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
 
 // // Configure the OTLP log exporter
@@ -10,6 +12,21 @@ import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
 //     'api-key': process.env.NEW_RELIC_LICENSE_KEY,
 //   },
 // });
+
+const logExporter = new OTLPLogExporter({
+  url: 'https://otlp.nr-data.net:4318/v1/logs', // New Relic OTLP endpoint
+  headers: {
+    'api-key': process.env.NEW_RELIC_LICENSE_KEY,
+  },
+});
+
+const logsProcessor = new SimpleLogRecordProcessor(logExporter);
+
+const traceExporter = new OTLPHttpProtoTraceExporter({
+  url: 'https://otel.highlight.io:4318/v1/traces',
+});
+
+const spanProcesor = new SimpleSpanProcessor(traceExporter);
 
 registerOTel({
   serviceName: process.env.NEW_RELIC_APP_NAME,
@@ -22,15 +39,8 @@ registerOTel({
     'highlight.project_id': process.env.PROJECT_ID,
     'highlight.source': 'backend',
   },
-  traceExporter: new OTLPHttpJsonTraceExporter({
-    url: 'https://otel.highlight.io:4318/v1/traces',
-  }),
-  spanProcessors: [
-    new BatchSpanProcessor(
-      new OTLPHttpJsonTraceExporter({
-        url: 'https://otel.highlight.io:4318/v1/traces',
-      }),
-    ),
-  ],
-  //  logRecordProcessor: new SimpleLogRecordProcessor(otlpLogExporter),
+  traceExporter: traceExporter,
+  spanProcessors: [spanProcesor],
+  // Pending fix https://github.com/sfishel18/vercel-otel/commit/d134528067900299d212072dfa1a9ed31e1ac653
+  //logRecordProcessor: logsProcessor
 });
