b966b4fd29f892e1d666c30e8f162862183ad074
jscontreras <sebastian.contreras@vercel.com>
2025-03-04 05:19:27 -0600

    Reducing noise on iinstrumentaton.

--- instrumentation.ts
+++ instrumentation.ts
@@ -3,7 +3,7 @@ import { registerOTel } from '@vercel/otel';
 export async function register() {
   // vercel means using @vercel/otel natively without NewRelic extension.
   // This way will trace middlware but no context for some reason
-  if (process.env.TELEMETRY_CUSTOM_PRODUCER === 'vercel') {
+  if (process.env.TELEMETRY_CUSTOM_PRODUCER === 'manual') {
     // For Node it supports open telemetry logs
     if (process.env.NEXT_RUNTIME === 'nodejs') {
       await import('./otel/instrumentation.vercel.node');
@@ -11,15 +11,6 @@ export async function register() {
     // Edge supports traces. (logs is WIP)
     if (process.env.NEXT_RUNTIME === 'edge') {
       await import('./otel/instrumentation.vercel.edge');
-    }
-  }
-  // sdk means using opentelemetry SDK (no vercel/otel)
-  else if (process.env.TELEMETRY_CUSTOM_PRODUCER === 'sdk') {
-    if (process.env.NEXT_RUNTIME === 'nodejs') {
-      await import('./otel/instrumentation.sdk.node');
-    }
-    if (process.env.NEXT_RUNTIME === 'edge') {
-      await import('./otel/instrumentation.sdk.edge');
     }
   }
   // By default it uses the credentials from newRelic-Vercel extension
