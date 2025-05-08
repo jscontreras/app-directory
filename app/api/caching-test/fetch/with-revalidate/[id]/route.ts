440ff2dffa62574c7325e6399770c375b473823f
jscontreras <sebastian.contreras@vercel.com>
2025-05-08 16:30:20 -0500

    testing revalidation effect over ISR and data-cache.

--- app/api/caching-test/fetch/with-revalidate/[id]/route.ts
+++ app/api/caching-test/fetch/with-revalidate/[id]/route.ts
@@ -3,17 +3,23 @@ import { NextRequest, NextResponse } from 'next/server';
 /**
  * This route is used to test the caching of the API.
  * It is a static route that is cached on the CDN and uses dynamic params.
- * The fetch cache is set to expire every 10 mins.
- * What happens when the Page is CDN revalidated? Is the fetch (data-cache) revalidated as well?
- * No, the fetch cache is not revalidated. STALE data is served until the new data is fetched but timeNYC will look
- * the same as the previous data is cached at the fetch level. The now paramter will change.
+ * This page CDN revalidates every minute
+ * The fetch cache is set to expire every 5 mins.
+ * The CDN cache is set to only expire when the fetch cache expires.
+ * What happens when the Page is CDN (path) on-demand revalidated? Is the fetch (data-cache) revalidated as well?
+ * Yes, the fetch cache is revalidated and a new snapshot is generated for the fetch cache.
+ * STALE data is served until the new data is fetched but timeNYC will change.
  * What happens when the fetch cache is revalidated? Is the Page revalidated as well?
  * Yes,the CDN is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
  * What happens when the fetch cache expires? Is the Page (CDN path) revalidated as well?
  * Yes, the CDN path is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
+ * What happens when the CDN cache expires? Is the fetch cache revalidated as well?
+ * No, the CDN cache will generate a new snapshot of the page but the fetch cache could retreive an old snapshot.
  */
+
 export const dynamic = 'force-static'; // static by default so CDN is enforced on TOP
-const dataCacheMins = 10;
+const dataCacheMins = 5;
+export const revalidate = 60;
 export async function GET(
   _request: NextRequest,
   {
@@ -43,6 +49,7 @@ export async function GET(
     second: 'numeric',
   });
   return NextResponse.json({
+    CDN: 'revalidates every minute',
     status: 'ok',
     timeNYC: currentTime,
     runtime: 'nodejs',
---
+++ app/api/caching-test/fetch/without-revalidate/[id]/route.ts
@@ -0,0 +1,62 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+/**
+ * This route is used to test the caching of the API.
+ * It is a static route that is cached on the CDN and uses dynamic params.
+ * The fetch cache is set to expire every 5 mins.
+ * The CDN cache is set to only expire when the fetch cache expires.
+ * What happens when the Page is CDN (path) on-demand revalidated? Is the fetch (data-cache) revalidated as well?
+ * Yes, the fetch cache is revalidated and a new snapshot is generated for the fetch cache.
+ * STALE data is served until the new data is fetched but timeNYC will change.
+ * What happens when the fetch cache is revalidated? Is the Page revalidated as well?
+ * Yes,the CDN is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
+ * What happens when the fetch cache expires? Is the Page (CDN path) revalidated as well?
+ * Yes, the CDN path is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
+ */
+
+export const dynamic = 'force-static'; // static by default so CDN is enforced on TOP
+const dataCacheMins = 5;
+
+export async function GET(
+  _request: NextRequest,
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  },
+) {
+  const { id } = await params;
+  const tags = ['api-caching-test', `api-caching-test-${id}`];
+  const res = await fetch('https://api.tc-vercel.dev/api/time', {
+    headers: {
+      'X-Custom-TC-Api-Key': process.env.CUSTOM_API_KEY || '',
+    },
+    cache: 'force-cache',
+    next: {
+      tags,
+      revalidate: 60 * dataCacheMins, // 10 mins
+    },
+  });
+  const data = (await res.json()) as { datetime: string };
+
+  const currentTime = new Date(data.datetime).toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  });
+  return NextResponse.json({
+    CDN: 'does not revalidate',
+    status: 'ok',
+    timeNYC: currentTime,
+    runtime: 'nodejs',
+    caches: [
+      'cdn-until-revalidate',
+      `fetch-data-cache-for-${dataCacheMins}-mins`,
+    ],
+    dataCache: 'fetch',
+    tags,
+    id,
+    now: Date.now(),
+  });
+}
