0a8a417c897e5f37b52eb8e66ee82f4a38ac8ace
jscontreras <sebastian.contreras@vercel.com>
2025-05-09 12:03:36 -0500

    Adding multiple params.

--- app/api/caching-test/fetch/without-revalidate/[slug]/[id]/route.ts
+++ app/api/caching-test/fetch/without-revalidate/[slug]/[id]/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 /**
  * This route is used to test the caching of the API.
  * It is a static route that is cached on the CDN and uses dynamic params.
- * This page CDN revalidates every minute
  * The fetch cache is set to expire every 5 mins.
  * The CDN cache is set to only expire when the fetch cache expires.
  * What happens when the Page is CDN (path) on-demand revalidated? Is the fetch (data-cache) revalidated as well?
@@ -13,23 +12,21 @@ import { NextRequest, NextResponse } from 'next/server';
  * Yes,the CDN is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
  * What happens when the fetch cache expires? Is the Page (CDN path) revalidated as well?
  * Yes, the CDN path is revalidated automatically. STALE data is served until the new data is stored in the fetch cache.
- * What happens when the CDN cache expires? Is the fetch cache revalidated as well?
- * No, the CDN cache will generate a new snapshot of the page but the fetch cache could retreive an old snapshot.
  */
 
 export const dynamic = 'force-static'; // static by default so CDN is enforced on TOP
 const dataCacheMins = 5;
-export const revalidate = 60;
+
 export async function GET(
   _request: NextRequest,
   {
     params,
   }: {
-    params: Promise<{ id: string }>;
+    params: Promise<{ id: string; slug: string }>;
   },
 ) {
-  const { id } = await params;
-  const tags = ['api-caching-test', `api-caching-test-${id}`];
+  const { id, slug } = await params;
+  const tags = ['api-caching-test', `api-caching-test-${slug}-${id}`];
   const res = await fetch('https://api.tc-vercel.dev/api/time', {
     headers: {
       'X-Custom-TC-Api-Key': process.env.CUSTOM_API_KEY || '',
@@ -49,7 +46,7 @@ export async function GET(
     second: 'numeric',
   });
   return NextResponse.json({
-    CDN: 'revalidates every minute',
+    CDN: 'does not revalidate',
     status: 'ok',
     timeNYC: currentTime,
     runtime: 'nodejs',
