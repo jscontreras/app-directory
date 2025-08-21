7b30b809b0ae536336a3858c80bcf7ad8da3a88a
jscontreras <sebastian.contreras@vercel.com>
2025-08-20 20:37:07 -0500

    Forcing dynamic, if static if loads and then hydrates.

--- app/streaming/node/product/[id]/page.tsx
+++ app/streaming/node/product/[id]/page.tsx
@@ -7,7 +7,7 @@ import { SingleProduct } from '#/app/streaming/_components/single-product';
 import { Ping } from '#/ui/ping';
 import { Suspense } from 'react';
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-dynamic';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
